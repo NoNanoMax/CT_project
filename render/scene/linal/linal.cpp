50dc1499c761040a2aaf79e931baae35aeacd9be
xxamxam <latypov.im@phystech.edu>
2022-03-05 22:09:25 +0300

    lights + linal update + camera

--- render/scene/linal/linal.cpp
+++ render/scene/linal/linal.cpp
@@ -17,8 +17,16 @@ Vec3::Vec3() {
 
 Vec3::Vec3(double x, double y, double z): x(x), y(y), z(z) { };
 
-double Vec3::abs() {
+double Vec3::abs() const{
     return sqrt(x * x + y * y + z * z);
+}
+
+bool Vec3::is_null() const{
+    return (x==0. and y == 0. and z == 0.);
+}
+
+bool Vec3::is_null() const{
+    return (x==0 or y == 0 or z == 0);
 }
 
 void Vec3::normalize() {
@@ -41,12 +49,27 @@ Vec3 Vec3::operator-(Vec3 other) {
     return Vec3(x - other.x, y - other.y, z - other.z);
 }
 
-Vec3 Vec3::operator*(double other) {
-    return Vec3(x * other, y * other, z * other);
+double operator*(const Vec3& a,const Vec3& b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+Vec3 operator*(const Vec3& a,const double b){
+    return Vec3(a.x*b, a.y*b, a.z*b);
+}
+
+Vec3 operator*(const double b,const Vec3& a){
+    return Vec3(a.x*b, a.y*b, a.z*b);
 }
 
 Vec3 Vec3::operator/(double other) {
     return Vec3(x / other, y / other, z / other);
+}
+
+double Vec3::operator[](int const& index) const{
+    if(index == 0) return x;
+    else if(index == 1) return y;
+    else if(index == 2) return z;
+    return 0;
 }
 
 double dot(Vec3 a, Vec3 b) {
--- render/scene/linal/linal.h
+++ render/scene/linal/linal.h
@@ -11,14 +11,20 @@ public:
     double x, y, z;
     Vec3();
     Vec3(double x, double y, double z);
-    double abs();
+    double abs() const;
+    bool is_null() const;
+    bool any_null() const;
     void normalize();
     Vec3 normalized();
     Vec3 operator+(Vec3 other);
     Vec3 operator-(Vec3 other);
-    Vec3 operator*(double other);
     Vec3 operator/(double other);
+    double operator[](int const& index) const;
 };
+
+double operator*(const Vec3& a,const Vec3& b);
+Vec3 operator*(const Vec3& a,const double b);
+Vec3 operator*(const double a,const Vec3& b);
 
 double dot(Vec3 a, Vec3 b);
 Vec3 vec(Vec3 a, Vec3 b);
