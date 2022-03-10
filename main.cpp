6aec7d00990ba49156fe11139649af25c258d752
NoNanoMax <danilov.ma@phystech.edu>
2022-03-11 01:04:00 +0300

    add vizualization

--- main.cpp
+++ main.cpp
@@ -15,13 +15,14 @@ int main() {
     r.read();
     r.render();
     Color** res = r.res;
-
-    for (int j = HEIGHT - 1; j >= 0; j--) {
+	
+    printf("%d %d %d\n", HEIGHT, WIDTH, 3);
+   	for (int j = HEIGHT - 1; j >= 0; j--) {
         for (int i = 0; i < WIDTH; i++) {
-            printf("%03d ", res[i][j].r);
+            printf("%03d %03d %03d ", res[i][j].r, res[i][j].r, res[i][j].r);
         }
-        std::cout << std::endl;
-    }
+   	}
+	std::cout << "\n";
 
     return 0;
 }
